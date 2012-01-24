003b78cc6b41bb72d944871759efa746fa4895a0
Deven Phillips <deven@dns.com>
2012-01-24 07:49:10 -0500

    Some refactoring and code cleanup.

--- src/com/dns/api/compiletime/GenericAPI.java
+++ src/com/dns/api/compiletime/GenericAPI.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.dns.api.compiletime;
 
 import java.io.BufferedReader;
@@ -22,15 +17,13 @@ import org.slf4j.LoggerFactory;
 
 /**
  *
- * @author Deven Phillips <deven.phillips@gmail.com>
+ * @author <a href="mailto: deven@dns.com">Deven Phillips</a>
  */
 public class GenericAPI {
 
 	protected String apiHost = null ;
 	protected boolean useSSL = false ;
 	protected String apiToken = null ;
-	protected String lastRequestURL = null ;
-	protected String lastRequestResult = null ;
 	protected Logger log = null ;
 
 	/**
@@ -76,14 +69,6 @@ public class GenericAPI {
 		return this.apiToken ;
 	}
 
-	public String getLastRequest() {
-		return lastRequestURL ;
-	}
-
-	public String getRawResults() {
-		return lastRequestResult ;
-	}
-
 	private String serializeStackTrace(StackTraceElement[] trace) {
 		StringBuilder traceBuilder = new StringBuilder() ;
 		for (int x=0; x<trace.length; x++) {
@@ -95,122 +80,137 @@ public class GenericAPI {
 	/**
 	 * Takes a protocol string and a URI and makes an HTTP request. Returns a parsed
 	 * <code>com.dns.mobile.json.JSONObject</code> from the results.
-	 * @param uri The portion of the URL after the host name
+	 * @param req The portion of the URL after the host name
 	 * @return A <code>com.dns.mobile.json.JSONObject</code> containing the results of the request.
 	 */
-	protected JSONObject makeHttpRequest(String uri) {
+	protected JSONObject makeHttpRequest(String req) {
 		String protocol = this.useSSL?"https":"http" ;
 		String breadCrumbs = "" ;
-		String apiCall = protocol+"://"+apiHost+uri;
-		JSONObject response = null ;
+		JSONObject response ;
 
-		HttpClient client = new DefaultHttpClient() ;
-		
-		HttpGet request = new HttpGet(apiCall) ;
-		request.addHeader("User-Agent", "DNS-Android") ;
-		HttpResponse answer = null ;
-		try {
-			log.debug("Sending request to "+protocol+"://"+apiHost+uri+"&isAndroid=true") ;
-			answer = client.execute(request) ;
-			log.debug("API HTTP Request Completed.") ;
-		} catch (ClientProtocolException cpe) {
-			log.debug("ClientProtocolException when trying to request API URL", cpe) ;
-			response = new JSONObject() ;
+		if (req!=null) {
+			String apiCall = protocol + "://" + apiHost + req;
+			response = null;
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet(apiCall);
+			request.addHeader("User-Agent", "DNS-Android");
+			HttpResponse answer = null;
 			try {
-				response.put("error", cpe.getLocalizedMessage());
-				response.put("breadcrumb", breadCrumbs) ;
-				response.put("stackTrace", serializeStackTrace(cpe.getStackTrace()));
-			} catch (JSONException jsone) {
-				log.error(cpe.getLocalizedMessage(), cpe) ;
-			}
-		} catch (IOException ioe) {
-			log.error("IOException when trying to request API URL", ioe) ;
-			response = new JSONObject() ;
-			try {
-				response.put("error", ioe.getLocalizedMessage());
-				response.put("breadcrumb", breadCrumbs) ;
-				response.put("stackTrace", serializeStackTrace(ioe.getStackTrace()));
-			} catch (JSONException jsone) {
-				log.error(jsone.getLocalizedMessage(), jsone) ;
-			}
-		}
-
-		boolean isValidStatus = false ;
-		if (answer!=null) {
-			if ((answer.getStatusLine().getStatusCode()>=200 && answer.getStatusLine().getStatusCode()<=220) || answer.getStatusLine().getStatusCode()==403) {
-				isValidStatus = true ;
-			}
-		} else {
-			log.error("The HTTP answer object is null!!") ;
-			response = new JSONObject() ;
-			try {
-				response.put("error", "HttpClient response is null.");
-				response.put("breadcrumb", breadCrumbs) ;
-			} catch (JSONException jsone) {
-				log.error(jsone.getLocalizedMessage(), jsone) ;
-			}
-		}
-
-		if (isValidStatus) {
-			log.debug("HTTP Response status is 200 OK") ;
-			try {
-				BufferedReader bis = new BufferedReader(new InputStreamReader(answer.getEntity().getContent())) ;
-				StringBuilder responseText = new StringBuilder() ;
-				String line = null ;
-				log.debug("Reading in the response body.") ;
-				while ((line = bis.readLine()) != null) {
-					responseText.append(line) ;
+				log.debug("Sending request to " + protocol + "://" + apiHost + req + "&isAndroid=true");
+				answer = client.execute(request);
+				log.debug("API HTTP Request Completed.");
+			} catch (ClientProtocolException cpe) {
+				log.debug("ClientProtocolException when trying to request API URL", cpe);
+				response = new JSONObject();
+				try {
+					response.put("error", cpe.getLocalizedMessage());
+					response.put("breadcrumb", breadCrumbs);
+					response.put("stackTrace", serializeStackTrace(cpe.getStackTrace()));
+				} catch (JSONException jsone) {
+					log.error(cpe.getLocalizedMessage(), cpe);
 				}
-				log.debug("Response body read and stored\n\n"+responseText.toString()+"\n") ;
-				response = new JSONObject(responseText.toString()) ;
 			} catch (IOException ioe) {
-				log.error("IOException when trying to read response body", ioe) ;
-				response = new JSONObject() ;
+				log.error("IOException when trying to request API URL", ioe);
+				response = new JSONObject();
 				try {
 					response.put("error", ioe.getLocalizedMessage());
-					response.put("breadcrumb", breadCrumbs) ;
+					response.put("breadcrumb", breadCrumbs);
 					response.put("stackTrace", serializeStackTrace(ioe.getStackTrace()));
 				} catch (JSONException jsone) {
-					log.error(jsone.getLocalizedMessage(), jsone) ;
+					log.error(jsone.getLocalizedMessage(), jsone);
 				}
-			} catch (JSONException jsone) {
-				log.error("JSONException encountered while parsing request body.", jsone) ;
-				response = new JSONObject() ;
+			}
+			boolean isValidStatus = false;
+			if (answer != null) {
+				if ((answer.getStatusLine().getStatusCode() >= 200 && answer.getStatusLine().getStatusCode() <= 220) || answer.getStatusLine().getStatusCode() == 403) {
+					isValidStatus = true;
+				}
+			} else {
+				log.error("The HTTP answer object is null!!");
+				response = new JSONObject();
 				try {
-					response.put("error", jsone.getLocalizedMessage());
-					response.put("breadcrumb", breadCrumbs) ;
-					response.put("stackTrace", serializeStackTrace(jsone.getStackTrace()));
-				} catch (JSONException jsone1) {
-					log.error(jsone1.getLocalizedMessage(), jsone1) ;
+					response.put("error", "HttpClient response is null.");
+					response.put("breadcrumb", breadCrumbs);
+				} catch (JSONException jsone) {
+					log.error(jsone.getLocalizedMessage(), jsone);
+				}
+			}
+			if (isValidStatus) {
+				log.debug("HTTP Response status is 200 OK");
+				try {
+					BufferedReader bis = new BufferedReader(new InputStreamReader(answer.getEntity().getContent()));
+					StringBuilder responseText = new StringBuilder();
+					String line = null;
+					log.debug("Reading in the response body.");
+					line = bis.readLine();
+					while (line != null) {
+						responseText.append(line);
+						line = bis.readLine();
+					}
+					log.debug("Response body read and stored\n\n" + responseText.toString() + "\n");
+					response = new JSONObject(responseText.toString());
+					bis.close() ;
+				} catch (IOException ioe) {
+					log.error("IOException when trying to read response body", ioe);
+					response = new JSONObject();
+					try {
+						response.put("error", ioe.getLocalizedMessage());
+						response.put("breadcrumb", breadCrumbs);
+						response.put("stackTrace", serializeStackTrace(ioe.getStackTrace()));
+					} catch (JSONException jsone) {
+						log.error(jsone.getLocalizedMessage(), jsone);
+					}
+				} catch (JSONException jsone) {
+					log.error("JSONException encountered while parsing request body.", jsone);
+					response = new JSONObject();
+					try {
+						response.put("error", jsone.getLocalizedMessage());
+						response.put("breadcrumb", breadCrumbs);
+						response.put("stackTrace", serializeStackTrace(jsone.getStackTrace()));
+					} catch (JSONException jsone1) {
+						log.error(jsone1.getLocalizedMessage(), jsone1);
+					}
+				}
+			} else {
+				log.debug("Status '" + answer.getStatusLine().getStatusCode() + "' was not valid");
+				BufferedReader bis = null;
+				try {
+					bis = new BufferedReader(new InputStreamReader(answer.getEntity().getContent()));
+				} catch (IllegalStateException e) {
+					log.error(e.getLocalizedMessage(), e);
+				} catch (IOException e) {
+					log.error(e.getLocalizedMessage(), e);
+				}
+				if (bis != null) {
+					StringBuilder responseText = new StringBuilder();
+					String line = null;
+					log.debug("Reading in the response body.");
+					try {
+						line = bis.readLine();
+						while (line != null) {
+							responseText.append(line);
+							line = bis.readLine();
+						}
+						bis.close() ;
+					} catch (IOException e) {
+						log.error(e.getLocalizedMessage(), e);
+					}
+					log.debug("Response body read and stored\n\n" + responseText.toString() + "\n");
+					response = new JSONObject();
+					try {
+						response.put("error", "HttpClient response has code '" + answer.getStatusLine().getStatusCode() + "'.");
+						response.put("breadcrumb", breadCrumbs);
+					} catch (JSONException jsone) {
+						log.error(jsone.getLocalizedMessage(), jsone);
+					}
 				}
 			}
 		} else {
-			log.debug("Status '"+answer.getStatusLine().getStatusCode()+"' was not valid") ;
-			BufferedReader bis = null ;
-			try {
-				bis = new BufferedReader(new InputStreamReader(answer.getEntity().getContent()));
-			} catch (IllegalStateException e) {
-				log.error(e.getLocalizedMessage(), e) ;
-			} catch (IOException e) {
-				log.error(e.getLocalizedMessage(), e) ;
-			}
-			StringBuilder responseText = new StringBuilder() ;
-			String line = null ;
-			log.debug("Reading in the response body.") ;
-			try {
-				while ((line = bis.readLine()) != null) {
-					responseText.append(line) ;
-				}
-			} catch (IOException e) {
-				log.error(e.getLocalizedMessage(), e) ;
-			}
-			log.debug("Response body read and stored\n\n"+responseText.toString()+"\n") ;
 			response = new JSONObject() ;
 			try {
-				response.put("error", "HttpClient response has code '"+answer.getStatusLine().getStatusCode()+"'.");
-				response.put("breadcrumb", breadCrumbs) ;
+				response.put("error", "The request string is null!!!") ;
 			} catch (JSONException jsone) {
-				log.error(jsone.getLocalizedMessage(), jsone) ;
+				log.error(jsone.getLocalizedMessage(), jsone);
 			}
 		}
 
